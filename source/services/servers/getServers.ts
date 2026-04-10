b2559bf580644c719ba3a28cdd55a1d39202a257
git-cameronbryce <developmentcameronbryce@gmail.com>
2026-04-10 10:16:43 -0600

    feat: add firestore and axios requests to fetch, filter, and return server data to main

---
+++ source/services/servers/getServers.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { base } from "../../config/constants";
+
+import type { Gameserver, Services } from "./types";
+
+const platforms: string[] = ["arkxb", "arkps"];
+
+// prettier-ignore
+const getGameserver = async (token: string, id: number): Promise<Gameserver> => {
+  const {
+    data: { data },
+  } = await axios.get(`${base}/services/${id}/gameservers`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+  return data.gameserver;
+};
+
+// prettier-ignore
+export const getServers = async (token: string): Promise<Gameserver[]> => {
+  const {
+    data: { data },
+  } = await axios.get(`${base}/services`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+  const filtered = data.services.filter((service: Services) =>
+    platforms.includes(service.details.folder_short),
+  );
+
+  return await Promise.all(
+    filtered.map((service: Services) => getGameserver(token, service.id)),
+  );
+};
