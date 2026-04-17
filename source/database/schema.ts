f6ed9c38ca9552e1ee37e85666fcb7677ace3e18
git-cameronbryce <developmentcameronbryce@gmail.com>
2026-04-17 10:09:44 -0600

    refactor: change drizzle schema structure

--- source/database/schema.ts
+++ source/database/schema.ts
@@ -1,18 +1,26 @@
-import { integer, pgTable, text } from "drizzle-orm/pg-core";
+import { boolean, integer, pgTable, text } from "drizzle-orm/pg-core";
 
 export const accountsTable = pgTable("accounts", {
-  guild: text().primaryKey(),
+  guild: text().primaryKey().notNull(),
   token: text().notNull(),
 });
 
 export const serversTable = pgTable("servers", {
   guild: text().references(() => accountsTable.guild),
-  id: integer().primaryKey(),
-  address: text(),
-  status: text(),
+  id: integer().primaryKey().notNull(),
+
   path: text(),
+  status: text(),
+  serverName: text(),
+  maximumPlayers: integer(),
+  currentPlayers: integer(),
+});
+
+export const playersTable = pgTable("players", {
+  serverId: integer().references(() => serversTable.id),
+  uuid: text().primaryKey(),
 
   name: text(),
-  slots: integer(),
-  players: integer(),
+  isOnline: boolean(),
+  lastOnline: text(),
 });
